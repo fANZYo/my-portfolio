878a2ff41dffd6fe52a8763ce970a397a2058f51
fANZYo <www.iehl@gmail.com>
2017-12-11 22:45:57 +0000

    changed port to 5000

--- src/pages/Blog/index.js
+++ src/pages/Blog/index.js
@@ -34,7 +34,7 @@ class Blog extends React.Component {
     });
 
     // CHANGE THIS LINE IN PRODUCTION
-    xhr.open('GET', 'http://localhost:8080/blogposts');
+    xhr.open('GET', 'http://localhost:5000/blogposts');
     // xhr.setRequestHeader('cache-control', 'no-cache');
 
     xhr.send(data);
